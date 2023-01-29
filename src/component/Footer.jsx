021424d899f603b93cfebcd7422fc6cc03d92400
hamitmizrak <hamitmizrak@gmail.com>
2023-01-29 18:27:39 +0300

    list

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,39 +4,31 @@ export default function Footer(props) {
   return (
     <>
   
-  <footer className="bg-dark text-center text-lg-start text-white">
 
+  <footer className="bg-dark text-center text-lg-start text-white mt-2 fixed-bottom">
   <div className="container p-4 pb-0">
     <form action="">
-    
       <div className="row">
-
         <div className="col-auto mb-4 mb-md-0">
           <p className="pt-2">
             <strong>Sign up for our newsletter</strong>
           </p>
         </div>
-     
+    
         <div className="col-md-5 col-12 mb-4 mb-md-0">
-         
           <div className="form-outline mb-4">
             <input type="email" id="form5Example25" className="form-control" />
             <label className="form-label" for="form5Example25">Email address</label>
           </div>
         </div>
-
         <div className="col-auto mb-4 mb-md-0">
-      
           <button type="submit" className="btn btn-primary mb-4">
             Subscribe
           </button>
         </div>
-       
       </div>
-
     </form>
   </div>
-
 
   <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
     © 2020 Copyright: {props.allright}
--- src/component/register/RegisterList.jsx
+++ src/component/register/RegisterList.jsx
@@ -1,9 +1,76 @@
 import React, { Component } from 'react'
+import RegisterService from '../../services/RegisterService';
 
+//classlar state bir yapıdadır. state yazarız: statefull
+//classlar this çok sever
 export default class RegisterList extends Component {
-  render() {
-    return (
-      <>RegisterList</>
-    )
-  }
-}
+
+    // constructor
+    constructor(props) {
+        super(props);
+        this.state = {
+            registerList: [],
+        }
+        //bind
+    } //end constructor
+
+    //function
+
+    //cdm:api servise erişim sağlamak
+    componentDidMount() {
+        RegisterService.apiRegisterList().then(
+            (response) => {
+                this.setState(
+                    {
+                        registerList: response.data
+                    }
+                )
+            }
+        )
+    }
+    //AJAX,SOCKET
+
+    //render
+    render() {
+        return (
+            <>
+                <h1 className="text-center display-4 text-primary">Register List</h1>
+                <table className="table table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>USERNAME</th>
+                            <th>SURNAME</th>
+                            <th>EMAİL</th>
+                            <th>PASSWORD</th>
+                            <th>TELEPHONE</th>
+                            <th>DATE</th>
+                            <th>Update</th>
+                            <th>View</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.registerList.map(temp => 
+                                <tr key={temp.id}>
+                                    <td>{temp.id}</td>
+                                    <td>{temp.username}</td>
+                                    <td>{temp.surname}</td>
+                                    <td>{temp.passwd}</td>
+                                    <td>{temp.email}</td>
+                                    <td>{temp.telephoneNumber}</td>
+                                    <td>{temp.createdDate}</td>
+                                    <td><i className="fa-solid fa-pen-to-square text-primary"></i></td>
+                                    <td><i className="fa-solid fa-tower-observation text-warning"></i></td>
+                                    <td><i className="fa-solid fa-trash text-danger"></i></td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </>
+        ) //end return
+    } // end render
+} //end class RegisterList
+
