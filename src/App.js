f82ffcf0851a9071bab1615d1a4a062af351175b
hamitmizrak <hamitmizrak@gmail.com>
2023-01-29 16:46:36 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Route, Router, Switch } from 'react-router-dom';
+
 import Header from './component/Header';
-import Main from './component/Main';
 import Footer from './component/Footer';
+
+import RegisterList from './component/register/RegisterList';
+import RegisterCreate from './component/register/RegisterCreate';
+import RegisterView from './component/register/RegisterView';
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Main/>
-    <Footer/>
-
-     
-    </>
+    <React.Fragment>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={RegisterList}></Route>
+          <Route path="/register-list" component={RegisterList}></Route>
+          <Route path="/register-add/:id" component={RegisterCreate}></Route>
+          <Route path="/register-view/:id" component={RegisterView}></Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </React.Fragment>
   );
 }
 
